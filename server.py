4cbc7f9796c5b37a1542587ee3448863fcd7b2f4
Melicodev <82504579+Melicodev@users.noreply.github.com>
2023-09-24 17:28:45 -0400

    created flask apps for each instance

--- server.py
+++ server.py
@@ -2,7 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/overall_app')
+def overall_app():
+    return 'Overall app !'
 
-@app.route('/')
-def my_app():
-    return 'Hello'
+
+@app.route('/cluster1')
+def cluster1_app():
+    return 'Cluster 1 is running successfully !'
+
+@app.route('/cluster2')
+def cluster2_app():
+    return 'Cluster 2 is running successfully !'
+
+if __name__=="__main__":
+    app.run(debug=True)
