3483f5a10fa6ea7c5e67c90ec29449a2b5660e83
Bananafly <andre@kreidemann.com>
2023-09-26 22:45:33 -0400

    failed last commit

--- instances.py
+++ instances.py
@@ -1,123 +1,123 @@
-#Python Program for creating a connection
-import os
+import time
 import boto3
 
-
-availabity_zones=['ap-south-1', 'eu-west-2', 'us-east-2', 'us-west-1', 'ap-northeast-2', 'eu-west-3',
-                  'ap-east-1', 'ca-central-1', 'ap-northeast-3', 'us-east-1']
 ec2_instances = []
-connections =[]
-instance_ids=[]
+instance_ids = []
 
-i=1
-def deploy_ec2():
-    global i
-    for zone in availabity_zones:
-        ec2 = boto3.client('ec2',
-                    zone)
+def launch_instances():
+    global ec2_instances
+    global instance_ids
+
+    # Initialize a session using the default configuration
+    session = boto3.Session(region_name='us-east-1')
+
+    # Initialize EC2 resource
+    ec2 = session.resource('ec2')
+
+    # Loop to create 10 instances
+    for i in range(9):
+        # Availability Zones
+        availability_zone = f'us-east-1{chr(97 + i % 3)}'  # 'us-east-1a', 'us-east-1b', 'us-east-1c' will be chosen in a round-robin manner
+        
+        # Create instance
+        instance = ec2.create_instances(
+            ImageId='ami-03a6eaae9938c858c',  # Update this to your desired AMI ID
+            MinCount=1,
+            MaxCount=1,
+            InstanceType="m4.large" if i % 2 else "t2.large",
+            KeyName='vockey',  # Update this to your key pair name
+            Placement={
+                'AvailabilityZone': availability_zone
+            }
+        )
+        
+        print(f"Launched instance {instance[0].id} in {availability_zone}")
+
         ec2_instances.append(ec2)
-        
-        id = ec2.Instance('id'+ i)
-        instance_ids.append(id)
-        i+=1
-        
-        
+        instance_ids.append(instance[0].id)
 
-    for index, instance in enumerate(ec2_instances) :
-        conn_1 = instance.run_instances(InstanceType="m4.large" if index % 2 else "t2.large",
-                            MaxCount=1,
-                            MinCount=1,
-                            ImageId="ami-02d55cb47e83a99a0") #Review the AMI-ID and choose it properly
-        connections.append(conn_1)
-
-    
 def terminate_ec2():
-    for i in len(ec2_instances):
-        response = ec2_instances[i].terminate_instances(InstanceIds=[instance_ids[i]])
+    global ec2_instances
+    global instance_ids
+
+    for i, ec2 in enumerate(ec2_instances):
+        response = ec2.meta.client.terminate_instances(InstanceIds=[instance_ids[i]])
+        print(f"Terminated instance {instance_ids[i]}")
+
+elbv2 = boto3.client('elbv2', region_name='us-east-1')
+
+def create_load_balancer():
+    response = elbv2.create_load_balancer(
+        Name='my-load-balancer',
+        Subnets=[
+            'subnet-069a250660a59a692',  # use a
+            'subnet-065ec3a3f43a694e2',  # use b
+        ],
+        SecurityGroups=[
+            'sg-0d21a4f0da1600c08',  # replace with your security group ID
+        ],
+        Scheme='internet-facing',
+        Tags=[
+            {
+                'Key': 'Name',
+                'Value': 'my-load-balancer'
+            },
+        ]
+    )
+    return response['LoadBalancers'][0]['LoadBalancerArn']
+
+def create_target_group():
+    response = elbv2.create_target_group(
+        Name='my-targets',
+        Protocol='HTTP',
+        Port=80,
+        VpcId='vpc-073a0b8c4bb5c3699',  # replace with your VPC ID
+        HealthCheckProtocol='HTTP',
+        HealthCheckPort='80',
+        HealthCheckPath='/',
+        HealthCheckIntervalSeconds=30,
+        HealthCheckTimeoutSeconds=5,
+        HealthyThresholdCount=5,
+        UnhealthyThresholdCount=2
+    )
+    return response['TargetGroups'][0]['TargetGroupArn']
+
+def register_targets(target_group_arn):
+    elbv2.register_targets(
+        TargetGroupArn=target_group_arn,
+        Targets=[
+            {
+                'Id': instance_id,
+            }
+            for instance_id in instance_ids
+        ]
+    )
+
+def create_listener(load_balancer_arn, target_group_arn):
+    elbv2.create_listener(
+        LoadBalancerArn=load_balancer_arn,
+        Protocol='HTTP',
+        Port=80,
+        DefaultActions=[
+            {
+                'Type': 'forward',
+                'TargetGroupArn': target_group_arn,
+            },
+        ]
+    )
 
 
-# # Initialize the AWS SDK
-# client = boto3.client('elbv2', region_name='us-east-1') #elastic load balancer version 2
-
-# # Creation of target groups for the two clusters:
-# response_cluster1 = client.create_target_group(
-#     Name='Cluster1TargetGroup',
-#     Protocol='HTTP',
-#     Port=80,
-#     TargetType='instance',
-#     VpcId='vpc-073a0b8c4bb5c3699',  # <-- Add your VPC ID here
-#     HealthCheckProtocol='HTTP',
-#     HealthCheckPath='/cluster1',        #URL cluster 1
-#     HealthCheckPort='traffic-port',
-# )
-
-# response_cluster2 = client.create_target_group(
-#     Name='Cluster2TargetGroup',
-#     Protocol='HTTP',
-#     Port=80,
-#     TargetType='instance',
-#     VpcId='vpc-073a0b8c4bb5c3699',  # <-- Add your VPC ID here xxx
-#     HealthCheckProtocol='HTTP',
-#     HealthCheckPath='/cluster2',        #URL cluster 2
-#     HealthCheckPort='traffic-port',
-# )
 
 
-# # Create an Application Load Balancer
-# response_alb = client.create_load_balancer(
-#     Name='AppLoadBalancer',
-#     Subnets=['subnet-069a250660a59a692', 'subnet-09ae9bb6a22db570d'],  # Replace with your subnet IDsxxx
-#     SecurityGroups=['sg-0d21a4f0da1600c08'],  # Replace with your security group IDsxxx
-#     Scheme='internet-facing',
-# )
-
-# # Create listener rules to route traffic to target groups
-# response_rule1 = client.create_listener_rule(
-#     ListenerArn=response_alb['LoadBalancers'][0]['ListenerArn'],
-#     Conditions=[
-#         {
-#             'Field': 'path-pattern',
-#             'Values': ['/cluster1'],            #URL matching target group 1
-#         },
-#     ],
-#     Priority=1,
-#     Action={
-#         'Type': 'fixed-response',
-#         'FixedResponseConfig': {
-#             'ContentType': 'text/plain',
-#             'StatusCode': '200',
-#             'ContentType': 'text/plain',
-#             'ContentDescription': 'm4.large instance ID',
-#             'ContentValue': 'm4-large-instance-id',  # Replace with the actual instance ID
-#         },
-#     },
-# )
-
-# response_rule2 = client.create_listener_rule(
-#     ListenerArn=response_alb['LoadBalancers'][0]['ListenerArn'],
-#     Conditions=[
-#         {
-#             'Field': 'path-pattern',
-#             'Values': ['/cluster2'],                #URL matching target group 2
-#         },
-#     ],
-#     Priority=2,
-#     Action={
-#         'Type': 'fixed-response',
-#         'FixedResponseConfig': {
-#             'ContentType': 'text/plain',
-#             'StatusCode': '200',
-#             'ContentType': 'text/plain',
-#             'ContentDescription': 't2.large instance ID',
-#             'ContentValue': 't2-large-instance-id',  # Replace with the actual instance ID
-#         },
-#     },
-# )
-
-# # Register EC2 instances to target groups
-# # Use boto3 to register instances to target groups
-
-# # Monitor performance using CloudWatch
-# # Set up CloudWatch Alarms and Metrics
-
-print("ALB and listener rules configured successfully.")
+if __name__ == '__main__':
+    launch_instances()
+    time.sleep(30)  # Wait for instances to start
+    
+    # Load Balancer part
+    load_balancer_arn = create_load_balancer()
+    time.sleep(30)  # Wait for load balancer to be active
+    
+    target_group_arn = create_target_group()
+    register_targets(target_group_arn)
+    create_listener(load_balancer_arn, target_group_arn)
+    
