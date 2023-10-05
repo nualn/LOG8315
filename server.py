658773b3ca304a0f9f23c3db887806ea4e744d8e
Bananafly <andre@kreidemann.com>
2023-10-05 15:20:56 -0400

    Docker Stuff (doesn't work right now, we need to Pass the cluster_number somehow

--- server.py
+++ server.py
@@ -2,18 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/overall_app')
+@app.route('/')
 def overall_app():
-    return 'Overall app !'
+    return f'Cluster {cluster_number} is running!'
 
 
-@app.route('/cluster1')
-def cluster1_app():
-    return 'Cluster 1 is running successfully !'
+# @app.route('/cluster1')
+# def cluster1_app():
+#     return 'Cluster 1 is running successfully !'
 
-@app.route('/cluster2')
-def cluster2_app():
-    return 'Cluster 2 is running successfully !'
+# @app.route('/cluster2')
+# def cluster2_app():
+#     return 'Cluster 2 is running successfully !'
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
