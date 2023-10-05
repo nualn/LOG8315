3bf3b868caedabb105b35572bf7aef538935e02a
Melicodev <82504579+Melicodev@users.noreply.github.com>
2023-10-04 20:59:11 -0400

    created script to run into the ec2 instances
    For the two clusters

---
+++ flask_clusters.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y install python3-pip 
+pip3 install flask
+mkdir flask_app 
+cd flask_app
+instance_id=$(ec2metadata --instance-id) 
+echo "from flask import Flask
+app = Flask(__name__)
+@app.route('/cluster1')
+def cluster1_app():
+    return 'Cluster 1 is running successfully !'
+
+@app.route('/cluster2')
+def cluster2_app():
+    return 'Cluster 2 is running successfully !'
+if __name__ == \"__main__\":
+       app.run(host='0.0.0.0', port=80) " | tee app.py
+python3 app.py 
