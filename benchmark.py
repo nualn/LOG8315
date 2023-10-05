f98cbfa41cdbbc23beafdeffb48b93df47b9f539
Melicodev <82504579+Melicodev@users.noreply.github.com>
2023-10-05 16:50:17 -0400

    Updated benchmark file to locally test it

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,6 @@
 import boto3
 from datetime import datetime, timedelta
+import main
 
 client = boto3.client('cloudwatch')
 
@@ -17,17 +18,17 @@ def get_target_group_metrics(load_balancer, target_group, metric_data):
                         'Dimensions': [
                             {
                                 'Name': 'target_group',
-                                'Value': 'string' #replace with target_group info
+                                'Value': target_group #replace with target_group info
                             },
                             {
                                 'Name': 'load_balancer',
-                                'Value': 'string' #replace with loead_balancer info
+                                'Value': load_balancer #replace with loead_balancer info
                             },
                         ]
                     },
-                    'Period': 123, #to change
+                    'Period': 600, #to change
                     'Stat': 'Average' if metric_data == 'TargetResponseTime' or metric_data == 'HealthyHostCount' or metric_data == 'UnHealthyHostCount' else 'Sum', #to define
-                    'Unit': 'Milliseconds'
+                    'Unit': 'Seconds'
                 }
             },
         ],
@@ -43,7 +44,7 @@ def get_load_balancer_metrics(load_balancer, metric_data):
     response = client.get_metric_data(
         MetricDataQueries=[
             {
-                'Id': 'metrics_for_tg', #giving a id name
+                'Id': 'metrics_for_elb', #giving a id name
                 'MetricStat': {
                     'Metric': {
                         'Namespace': 'AWS/ApplicationELB',
@@ -51,14 +52,15 @@ def get_load_balancer_metrics(load_balancer, metric_data):
                         'Dimensions': [
                             {
                                 'Name': 'load_balancer',
-                                'Value': 'string' #replace with loead_balancer info
+                                'Value': load_balancer #replace with loead_balancer info
                             },
                         ]
                     },
                     'Period': 123, #to change
-                    'Stat': 'Average' if metric_data == 'TargetResponseTime' or metric_data == 'HealthyHostCount' or metric_data == 'UnHealthyHostCount' else 'Sum', #to define
+                    'Stat': 'Average' if metric_data == 'TargetResponseTime'  else 'Sum', #to define
                     'Unit': 'Milliseconds'
-                }
+                },
+                'ReturnData': True,
             },
         ],
         StartTime=datetime.utcnow() - timedelta(minutes=15),
@@ -66,3 +68,18 @@ def get_load_balancer_metrics(load_balancer, metric_data):
     
     )
     return response, average
+
+if __name__ == "__main__":
+    
+    metric_data_tg=['RequestCount', 'HealthyHostCount', 'UnHealthyHostCount', 
+                 'HTTPCode_Target_2XX', 'HTTPCode_Target_3XX', 'HTTPCode_Target_4XX', 
+                 'HTTPCode_Target_5XX', 'TargetResponseTime' ]
+    metric_data_elb=['RequestCount', 'HTTPCode_ELB_4XX', 'HTTPCode_ELB_5XX', 'TargetResponseTime']
+    
+    #test : 
+    load_balancer_arn= "arn:aws:elasticloadbalancing:us-east-1:537844504549:loadbalancer/app/my-load-balancer/c2c28914c344ba00"
+    target_group_arns= ['arn:aws:elasticloadbalancing:us-east-1:537844504549:targetgroup/tgcluster1/5a064c17f37dec5e',
+                           'arn:aws:elasticloadbalancing:us-east-1:537844504549:targetgroup/tgcluster2/288853aefefb93ec']
+    
+    response, average = get_target_group_metrics(load_balancer_arn, target_group_arns[0],metric_data_tg[0])
+    print(response, average)
