2da60608fd3db3dcb8886e8c4a06f3b86dd0a29c
Melicodev <82504579+Melicodev@users.noreply.github.com>
2023-10-05 20:16:05 -0400

    generated benchmark code working

--- benchmark.py
+++ benchmark.py
@@ -1,42 +1,58 @@
 import boto3
 from datetime import datetime, timedelta, timezone
+metric_data_tg=['RequestCount', 'HealthyHostCount', 'UnHealthyHostCount', 
+                 'HTTPCode_Target_2XX', 'HTTPCode_Target_3XX', 'HTTPCode_Target_4XX', 
+                 'HTTPCode_Target_5XX', 'TargetResponseTime' ]
 
-
-def get_target_response_time():
+def get_target_response_time(load_balancer_arn, target_group_arn, metric_data):
     client = boto3.client('cloudwatch')
-
-    response = client.get_metric_data(
-        MetricDataQueries=[
-            {
-                'Id': 'a1',
-                'MetricStat': {
-                    'Metric': {
-                        'Namespace': 'AWS/ApplicationELB',
-                        'MetricName': 'TargetResponseTime',
-                        'Dimensions': [
-                            {
-                                'Name': 'TargetGroup',
-                                'Value': 'targetgroup/tgcluster1/b43bdcca7b749934',
+    load_balancer_split=load_balancer_arn.split('/')
+    load_balancer_split=load_balancer_split[1]+'/'+load_balancer_split[2]+'/'+load_balancer_split[3]
+    target_group_metrics={}
+    for target_group in target_group_arn:
+        target_group_metrics[target_group]={}
+        target_split=target_group.split(':')
+        target_split=target_split[-1]
+        for metric in metric_data:
+            response = client.get_metric_data(
+                MetricDataQueries=[
+                    {
+                        'Id': 'a1',
+                        'MetricStat': {
+                            'Metric': {
+                                'Namespace': 'AWS/ApplicationELB',
+                                'MetricName': metric,
+                                'Dimensions': [
+                                    {
+                                        'Name': 'TargetGroup',
+                                        'Value': target_split,
+                                    },
+                                    {
+                                        'Name': 'LoadBalancer',
+                                        'Value': load_balancer_split,
+                                    },
+                                ]
                             },
-                            {
-                                'Name': 'LoadBalancer',
-                                'Value': 'app/my-load-balancer/22eaf66bd3d4c236',
-                            },
-                        ]
+                            'Period': 60,
+                            'Stat': 'Average' if metric == 'TargetResponseTime' or metric == 'HealthyHostCount' or metric == 'UnHealthyHostCount' else 'Sum',
+                            'Unit': 'Seconds' if metric == 'TargetResponseTime' else 'Count'
+                        },
+                        'ReturnData': True,
                     },
-                    'Period': 60,
-                    'Stat': 'Average',
-                },
-                'ReturnData': True,
-            },
-        ],
-        StartTime=datetime.now(timezone.utc) - timedelta(hours=2),
-        EndTime=datetime.now(timezone.utc)
-    )
-    return list(zip(response['MetricDataResults'][0]['Timestamps'], response['MetricDataResults'][0]['Values']))
+                ],
+                StartTime=datetime.now(timezone.utc) - timedelta(hours=2),
+                EndTime=datetime.now(timezone.utc)
+            )
+            target_group_metrics[target_group][metric]=list(zip(response['MetricDataResults'][0]['Timestamps'], response['MetricDataResults'][0]['Values']))
+    return target_group_metrics
 
 
-if __name__ == '__main__':
+"""if __name__ == '__main__':
+    
+    load_balancer_arn= "arn:aws:elasticloadbalancing:us-east-1:537844504549:loadbalancer/app/my-load-balancer/75dc4d0c5afd3f9b"
+    target_group_arns= ['arn:aws:elasticloadbalancing:us-east-1:537844504549:targetgroup/tgcluster1/5a064c17f37dec5e',
+                           'arn:aws:elasticloadbalancing:us-east-1:537844504549:targetgroup/tgcluster2/288853aefefb93ec']
 
-    print(datetime.now(timezone.utc) - timedelta(hours=2))
-    print(get_target_response_time())
+    
+    #print(datetime.now(timezone.utc) - timedelta(hours=2))
+    print(get_target_response_time(load_balancer_arn, target_group_arns, metric_data_tg))"""
