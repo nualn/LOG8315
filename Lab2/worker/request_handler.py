c5b06afd992f7e12104f623d762cc1c03ebb6e59
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2023-10-31 07:39:22 +0200

    Add worker flask server

---
+++ Lab2/worker/request_handler.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+import torch
+import random
+import string
+
+app = Flask(__name__)
+
+# Load the pre-trained model and tokenizer
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+model = DistilBertForSequenceClassification.from_pretrained(
+    'distilbert-base-uncased', num_labels=2)
+
+
+def generate_random_text(length=50):
+    letters = string.ascii_lowercase + ' '
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+@app.route('/run_model', methods=['POST'])
+def run_model():
+    # Generate random input text
+    input_text = generate_random_text()
+
+    # Tokenize the input text and run it through the model
+    inputs = tokenizer(input_text, return_tensors='pt',
+                       padding=True, truncation=True)
+    outputs = model(**inputs)
+
+    # The model returns logits, so let's turn that into probabilities
+    probabilities = torch.softmax(outputs.logits, dim=-1)
+
+    # Convert the tensor to a list and return
+    probabilities_list = probabilities.tolist()[0]
+
+    return jsonify({"input_text": input_text, "probabilities": probabilities_list})
+
+
+if __name__ == '__main__':
+    # Adjust the port as needed for your setup
+    app.run(host='0.0.0.0', port=5000)
