76463aea7eded5c1632819ab52df4e620856bbcd
Melicodev <82504579+Melicodev@users.noreply.github.com>
2023-11-06 11:02:32 -0500

    made a function for make requests

--- Lab2/request_sender.py
+++ Lab2/request_sender.py
@@ -4,9 +4,16 @@ import requests
 def send_request(url):
     response = requests.get(url)
     print(f"Response from the cluster: {response.text}")
+    
 
-if __name__ == "__main__":
-    url=""
+def make_requests(url):
+    #I must get the cluster's url
     num_requests = 5 
     with multiprocessing.Pool(processes=num_requests) as pool: # it is going to create a pool of worker processes that will send requests simultaneously
         pool.map(send_request, range(num_requests))
+
+"""if __name__ == "__main__":
+    url="" #I must get the cluster's url
+    num_requests = 5 
+    with multiprocessing.Pool(processes=num_requests) as pool: # it is going to create a pool of worker processes that will send requests simultaneously
+        pool.map(send_request, range(num_requests))"""
