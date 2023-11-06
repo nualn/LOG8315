c43f28872ae75880cfdf03253060b1021ba78272
Melicodev <82504579+Melicodev@users.noreply.github.com>
2023-11-06 18:13:52 -0500

    debugged my function now it works

--- Lab2/request_sender.py
+++ Lab2/request_sender.py
@@ -1,19 +1,27 @@
 import multiprocessing
 import requests
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+
+
 
 def send_request(url):
-    response = requests.get(url)
-    print(f"Response from the cluster: {response.text}")
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check for HTTP errors
+        print(f"Response from the cluster: {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending request to {url}: {e}")
     
 
-def make_requests(url):
-    #I must get the cluster's url
-    num_requests = 5 
-    with multiprocessing.Pool(processes=num_requests) as pool: # it is going to create a pool of worker processes that will send requests simultaneously
-        pool.map(send_request, range(num_requests))
 
-"""if __name__ == "__main__":
-    url="" #I must get the cluster's url
-    num_requests = 5 
+
+if __name__ == "__main__":
+    url="http://3.82.195.205/cluster" #I must get the orchestrator's url and add /cluster
+
+    num_requests = 5
     with multiprocessing.Pool(processes=num_requests) as pool: # it is going to create a pool of worker processes that will send requests simultaneously
-        pool.map(send_request, range(num_requests))"""
+        pool.map(send_request, [url] * num_requests)
+
